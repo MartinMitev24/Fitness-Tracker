3118e6af7d674451236b47a3da6331895242c2b8
Martin Mitev <martin.mitev.2403@gmail.com>
2024-10-07 20:22:55 +0300

    Created Exercise class.

---
+++ Fitness-Tracker.Infrastructure/Data/Models/Exercise.cs
@@ -0,0 +1,30 @@
+﻿using Fitness_Tracker.Infrastructure.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static Fitness_Tracker.Infrastructure.Data.Constants.DataConstants;
+
+namespace Fitness_Tracker.Infrastructure.Data.Models
+{
+    [Comment("Exercise")]
+    public class Exercise
+    {
+        [Key]
+        [Comment("Exercise identifier")]
+        public int Id { get; init; }
+
+        [Required]
+        [MaxLength(ExerciseNameMaxLength)]
+        [Comment("Exercise name, can be of maximum 50 characters")]
+        public required string ExerciseName { get; set; }
+
+        [Required]
+        [MaxLength(ExerciseDescriptionMaxLength)]
+        [Comment("Exercise description, can be of maximum 1000 characters")]
+
+        public required string ExerciseDescription { get; set; }
+
+        [Required]
+        [Comment("Exercise muscle group")]
+        public TargetMuscleGroup TargetMuscleGroup { get; set; }
+    }
+}
