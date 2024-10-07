d56b5acfafa887deb3dcc37fd14e96603a7930d5
Martin Mitev <martin.mitev.2403@gmail.com>
2024-10-07 20:22:34 +0300

    Created enum TargetMuscleGroup.cs, used to specify what muscles the exercise is working.

---
+++ Fitness-Tracker.Infrastructure/Data/Enums/TargetMuscleGroup.cs
@@ -0,0 +1,15 @@
+﻿namespace Fitness_Tracker.Infrastructure.Data.Enums
+{
+    public enum TargetMuscleGroup
+    {
+        None = 0,
+        Chest = 1,
+        Back = 2,
+        Shoulder = 3,
+        Biceps = 4,
+        Triceps = 5,
+        Abdominal = 6,
+        Legs = 7,
+        Compound = 8,
+    }
+}
