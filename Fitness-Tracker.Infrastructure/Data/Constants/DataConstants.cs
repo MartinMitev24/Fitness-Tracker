a7929bc23d20550686d482a96e33167e3d147060
Martin Mitev <martin.mitev.2403@gmail.com>
2024-10-07 20:20:48 +0300

    Created DataConstants

---
+++ Fitness-Tracker.Infrastructure/Data/Constants/DataConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Fitness_Tracker.Infrastructure.Data.Constants
+{
+    public static class DataConstants
+    {
+        public const int ExerciseNameMaxLength = 50;
+        public const int ExerciseNameMinLength = 5;
+
+        public const int ExerciseDescriptionMaxLength = 1000;
+        public const int ExerciseDescriptionMinLength = 5;
+    }
+}
