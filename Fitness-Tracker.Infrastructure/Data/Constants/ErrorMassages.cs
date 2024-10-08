7ef588fe1d01d0b3a2ec24f3477c84449d301152
Martin Mitev <martin.mitev.2403@gmail.com>
2024-10-08 19:53:45 +0300

    Removed unnecessary using from class.

--- Fitness-Tracker.Infrastructure/Data/Constants/ErrorMassages.cs
+++ Fitness-Tracker.Infrastructure/Data/Constants/ErrorMassages.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static Fitness_Tracker.Infrastructure.Data.Constants.DataConstants;
+﻿using static Fitness_Tracker.Infrastructure.Data.Constants.DataConstants;
 
 
 namespace Fitness_Tracker.Infrastructure.Data.Constants
