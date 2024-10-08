16193325705e1196c21603e551f05ea21ad424ba
Martin Mitev <martin.mitev.2403@gmail.com>
2024-10-08 19:38:02 +0300

    Added ErrorMassages.cs

---
+++ Fitness-Tracker.Infrastructure/Data/Constants/ErrorMassages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Fitness_Tracker.Infrastructure.Data.Constants.DataConstants;
+
+
+namespace Fitness_Tracker.Infrastructure.Data.Constants
+{
+    public class ErrorMassages
+    {
+        public string ExerciseNameErrorMassage = $"Exercise name can be between {ExerciseNameMinLength} and {ExerciseNameMaxLength}";
+        public string ExerciseDescriptionErrorMassage = $"Exercise name can be between {ExerciseDescriptionMinLength} and {ExerciseDescriptionMaxLength}";
+        
+    }
+}
