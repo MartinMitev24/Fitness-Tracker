f4e51a2ed9f60b70a262ca9c4aeaded4816764a1
Martin Mitev <martin.mitev.2403@gmail.com>
2024-10-12 15:50:08 +0300

    Added IExerciseService.cd and ExerciseService.cs to project

---
+++ Fitness-Tracker.Core/Contracts/IExerciseService.cs
@@ -0,0 +1,14 @@
+﻿using Fitness_Tracker.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_Tracker.Core.Contracts
+{
+    public interface IExerciseService
+    {
+        Task<IEnumerable<ExerciseViewModel>> GetAllAsync();
+    }
+}
---
+++ Fitness-Tracker.Core/Services/ExerciseService.cs
@@ -0,0 +1,31 @@
+﻿using Fitness_Tracker.Core.Contracts;
+using Fitness_Tracker.Core.Models;
+using Fitness_Tracker.Infrastructure.Data.Common;
+using Fitness_Tracker.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitness_Tracker.Core.Services
+{
+    public class ExerciseService : IExerciseService
+    {
+        private readonly IRepository _repository;
+
+        public ExerciseService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<ExerciseViewModel>> GetAllAsync()
+        {
+            return await _repository.All<Exercise>()
+                .Select(e => new ExerciseViewModel
+                {
+                    Id = e.Id,
+                    ExerciseName = e.ExerciseName,
+                    ExerciseDescription = e.ExerciseDescription,
+                    TargetMuscleGroup = e.TargetMuscleGroup
+                })
+                .ToListAsync();
+        }
+    }
+}
