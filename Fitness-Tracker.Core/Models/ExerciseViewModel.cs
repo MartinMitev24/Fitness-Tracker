54ca38841177fb9686c3373db41e61634134571b
Martin Mitev <martin.mitev.2403@gmail.com>
2024-10-12 14:49:39 +0300

    Removed unnecessery data anotations and usings.

--- Fitness-Tracker.Core/Models/ExerciseViewModel.cs
+++ Fitness-Tracker.Core/Models/ExerciseViewModel.cs
@@ -1,6 +1,4 @@
 ﻿using Fitness_Tracker.Infrastructure.Data.Enums;
-using System.ComponentModel.DataAnnotations;
-using static Fitness_Tracker.Infrastructure.Data.Constants.DataConstants;
 
 namespace Fitness_Tracker.Core.Models
 {
@@ -14,21 +12,16 @@ namespace Fitness_Tracker.Core.Models
         /// <summary>
         /// Name of the exercise.
         /// </summary>
-        [Required]
-        [StringLength(ExerciseNameMaxLength, MinimumLength = ExerciseNameMinLength)]
         public required string ExerciseName { get; set; }
 
         /// <summary>
         /// Description of the exercise.
         /// </summary>
-        [Required]
-        [StringLength(ExerciseDescriptionMaxLength, MinimumLength = ExerciseDescriptionMinLength)]
         public required string ExerciseDescription { get; set; }
 
         /// <summary>
         /// Integer value for muscle groupe of the exercice. 0=None, 1=Chest, 2=Back, 3=Shoulder, 4=Biceps, 5=Triceps, 6=Abdominal, 7=Legs and8=Compound
         /// </summary>
-        [Required]
         public TargetMuscleGroup TargetMuscleGroup { get; set; }
     }
 }
