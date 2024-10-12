3def918ed2e0f301451521825c53f30431033ec1
Martin Mitev <martin.mitev.2403@gmail.com>
2024-10-12 15:52:41 +0300

    Created All Exercise View

---
+++ Fitness-Tracker/Controllers/ExerciseController.cs
@@ -0,0 +1,24 @@
+﻿using Fitness_Tracker.Core.Contracts;
+using Fitness_Tracker.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fitness_Tracker.Controllers
+{
+    public class ExerciseController : Controller
+    {
+        private readonly IExerciseService _exerciseService;
+
+        public ExerciseController(IExerciseService exerciseService)
+        {
+            _exerciseService = exerciseService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            IEnumerable<ExerciseViewModel> model = await _exerciseService.GetAllAsync();
+
+            return View(model);
+        }
+    }
+}
