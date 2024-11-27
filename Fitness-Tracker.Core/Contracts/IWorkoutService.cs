b22307ec729ed647576297dfeeb8d40c42b6a105
Martin Mitev <martin.mitev.2403@gmail.com>
2024-11-27 19:28:48 +0200

    Added IWorkoutService Interface and WorkoutService Class

---
+++ Fitness-Tracker.Core/Contracts/IWorkoutService.cs
@@ -0,0 +1,22 @@
+﻿using Fitness_Tracker.Core.Models.Workout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_Tracker.Core.Contracts
+{
+    internal interface IWorkoutService
+    {
+        Task<IEnumerable<WorkoutViewModel>> GetAllAsync();
+
+        Task<WorkoutViewModel> FindExercise(int id);
+
+        //Task<EditExerciseFormModel> GetExercise(int id);
+
+        //Task EditExercise(EditExerciseFormModel model);
+
+        //Task CreateExercise(AddExerciseFormModel model);
+    }
+}
---
+++ Fitness-Tracker.Core/Services/WorkoutService.cs
@@ -0,0 +1,31 @@
+﻿using Fitness_Tracker.Core.Contracts;
+using Fitness_Tracker.Core.Models.Workout;
+using Fitness_Tracker.Infrastructure.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_Tracker.Core.Services
+{
+    internal class WorkoutService : IWorkoutService
+    {
+        private readonly IRepository _repository;
+
+        public WorkoutService(var repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<WorkoutViewModel> FindExercise(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<WorkoutViewModel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
