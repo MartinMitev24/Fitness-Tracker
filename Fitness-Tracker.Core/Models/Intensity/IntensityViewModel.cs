a0a9f7fd774526cd27a507bbed0d7e5056823161
Martin Mitev <martin.mitev.2403@gmail.com>
2024-11-27 19:40:34 +0200

    Created IntensityViewModel to Models

---
+++ Fitness-Tracker.Core/Models/Intensity/IntensityViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fitness_Tracker.Core.Models.Intensity
+{
+    public class IntensityViewModel
+    {
+        public int Id { get; set; }
+
+        public int LiftedWeight { get; set; }
+
+        public int Reps { get; set; }
+
+        public int Sets { get; set; }
+
+        public int AvarageTimePerSet { get; set; }
+    }
+}
