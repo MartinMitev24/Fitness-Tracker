f9562528bea20ebcc1ef72a2f640447d58d44b68
Martin Mitev <martin.mitev.2403@gmail.com>
2024-11-30 10:24:55 +0200

    Created IIntensityService and IntensityService

---
+++ Fitness-Tracker.Core/Contracts/IIntensityService.cs
@@ -0,0 +1,17 @@
+﻿using Fitness_Tracker.Core.Models.Intensity;
+
+namespace Fitness_Tracker.Core.Contracts
+{
+    public interface IIntensityService
+    {
+        Task<IEnumerable<IntensityViewModel>> GetAllAsync();
+
+        Task<IntensityViewModel> FindIntensity(int id);
+
+        //Task<EditExerciseFormModel> GetExercise(int id);
+
+        //Task EditExercise(EditExerciseFormModel model);
+
+        //Task CreateExercise(AddExerciseFormModel model);
+    }
+}
---
+++ Fitness-Tracker.Core/Services/IntensityService.cs
@@ -0,0 +1,45 @@
+﻿using Fitness_Tracker.Core.Contracts;
+using Fitness_Tracker.Core.Models.Intensity;
+using Fitness_Tracker.Infrastructure.Data.Common;
+using Fitness_Tracker.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_Tracker.Core.Services
+{
+    public class IntensityService : IIntensityService
+    {
+        private readonly IRepository _repository;
+
+        public IntensityService(IRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IEnumerable<IntensityViewModel>> GetAllAsync()
+        {
+            IEnumerable<IntensityViewModel> intensities = await _repository.AllReadOnly<Intensity>()
+                .Select(i => new IntensityViewModel
+                {
+                    Id = i.Id,
+                    ExerciseName = i.Exercise.ExerciseName,
+                    LiftedWeight = i.LiftedWeight,
+                    Reps = i.Reps,
+                    Sets = i.Sets,
+                    AvarageTimePerSet = i.AvarageTimePerSet,
+                    WorkoutId = i.WorkoutId
+                })
+                .ToListAsync();
+
+            return intensities;
+        }
+
+        public Task<IntensityViewModel> FindIntensity(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
