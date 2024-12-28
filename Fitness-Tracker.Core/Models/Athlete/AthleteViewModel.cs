9122748b68ee857e9880c7b437eb911767ee8b9d
Martin Mitev <martin.mitev.2403@gmail.com>
2024-12-28 12:50:01 +0200

    Created AthleteViewModel

---
+++ Fitness-Tracker.Core/Models/Athlete/AthleteViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_Tracker.Core.Models.Athlete
+{
+    public class AthleteViewModel
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
