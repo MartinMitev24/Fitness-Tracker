e03ffbab54e9d51d2f4969a0aaa00523d5e61eeb
Martin Mitev <martin.mitev.2403@gmail.com>
2025-02-09 10:23:39 +0200

    Created JQuery for modal to post intensity data in new workout view.

---
+++ Fitness-Tracker/wwwroot/js/jquery.add.exercise.js
@@ -0,0 +1,33 @@
+﻿$('#addExerciseForm').submit(function (e) {
+    e.preventDefault();
+
+    var exerciseId = $('#exerciseId').val();
+    var weight = $('#weight').val();
+    var reps = $('#reps').val();
+    var sets = $('#sets').val();
+    var time = $('#time').val();
+
+    console.log(exerciseId);
+    console.log(weight);
+    console.log(reps);
+    console.log(sets);
+    console.log(time);
+
+    $.ajax({
+        url: addExerciseUrl,
+        type: 'POST',
+        data: {
+            exerciseId: exerciseId,
+            weight: weight,
+            reps: reps,
+            sets: sets,
+            time: time
+        },
+        success: function (respons) {
+            $('#addExerciseToWorkout').modal('hide');
+            location.reload();
+        },
+        error: function (xhr, status, error) {
+        }
+    });
+});
