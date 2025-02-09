283c5d767fe8d9b06ac0bab763571d89585dac55
Martin Mitev <martin.mitev.2403@gmail.com>
2025-02-09 10:17:12 +0200

    Created class "ExerciseToChooseViewModel"

---
+++ Fitness-Tracker.Core/Models/Workout/ExerciseToChooseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Fitness_Tracker.Core.Models.Workout
+{
+    /// <summary>
+    /// Class is used for choosing an exercise for IntensityFormModel when creating a new workout.
+    /// </summary>
+    public class ExerciseToChooseViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string MuscleType { get; set; } = string.Empty;
+    }
+}
