22c4f5cac9501d3dd6558f763e7751865320b870
Martin Mitev <martin.mitev.2403@gmail.com>
2025-02-09 10:13:51 +0200

    Created class "IntensityFormModel" and added it to "_viewImports.cshtml".

---
+++ Fitness-Tracker.Core/Models/Intensity/IntensityFormModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Fitness_Tracker.Core.Models.Intensity
+{
+    /// <summary>
+    /// Class is used to create temporary data for performed exersices in workouts.
+    /// </summary>
+    public class IntensityFormModel
+    {
+        public int ExerciseId { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Reps { get; set; }
+
+        public int Sets { get; set; }
+
+        public int Time { get; set; }
+    }
+}
