d1241fe33c4d452b07d2daefd6c2949e51bf5a44
Martin Mitev <martin.mitev.2403@gmail.com>
2025-02-09 10:18:13 +0200

    Created class "WorkoutFormModel"

---
+++ Fitness-Tracker.Core/Models/Workout/WorkoutFormModel.cs
@@ -0,0 +1,15 @@
+﻿using Fitness_Tracker.Core.Models.Intensity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fitness_Tracker.Core.Models.Workout
+{
+    /// <summary>
+    /// Class is used for creating a new Workout.
+    /// </summary>
+    public class WorkoutFormModel
+    {
+        public int AthleteId { get; set; }
+
+        public IEnumerable<IntensityFormModel> Intensities { get; set; } = new List<IntensityFormModel>();
+    }
+}
