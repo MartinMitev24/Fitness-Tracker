2a45c0a67257b34468cfcc73752ae9b63e39408e
Martin Mitev <martin.mitev.2403@gmail.com>
2025-03-08 09:05:53 +0200

    Added comments to class IntensityViewModel.

--- Fitness-Tracker.Core/Models/Intensity/IntensityViewModel.cs
+++ Fitness-Tracker.Core/Models/Intensity/IntensityViewModel.cs
@@ -4,20 +4,44 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Fitness_Tracker.Core.Models.Intensity
 {
+    /// <summary>
+    /// Class is used for viewing performed exersices data in workouts.
+    /// </summary>
     public class IntensityViewModel
     {
+        /// <summary>
+        /// Intensity Identifier.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Performed Exercise name.
+        /// </summary>
         public string ExerciseName { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Integer for the weight used in the exercise.
+        /// </summary>
         public int LiftedWeight { get; set; }
 
+        /// <summary>
+        /// Integer for the number repetitions performed for the exercise.
+        /// </summary>
         public int Reps { get; set; }
 
+        /// <summary>
+        /// Integer for the number of performed sets. 
+        /// </summary>
         public int Sets { get; set; }
 
+        /// <summary>
+        /// Integer for the avarage time spent on one set.
+        /// </summary>
         public int AvarageTimePerSet { get; set; }
 
+        /// <summary>
+        /// Workout Identifier associated with the performed exercise.
+        /// </summary>
         public int WorkoutId { get; set; }
     }
 }
