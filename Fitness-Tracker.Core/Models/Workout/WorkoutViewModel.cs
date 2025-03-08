da1fe1d927b0bf27651d02fb796df98823620808
Martin Mitev <martin.mitev.2403@gmail.com>
2025-03-08 08:41:52 +0200

    Added comments to class WorkoutViewModel.

--- Fitness-Tracker.Core/Models/Workout/WorkoutViewModel.cs
+++ Fitness-Tracker.Core/Models/Workout/WorkoutViewModel.cs
@@ -2,12 +2,24 @@
 
 namespace Fitness_Tracker.Core.Models.Workout
 {
+    /// <summary>
+    /// Class is used for viewing athlete workouts.
+    /// </summary>
     public class WorkoutViewModel
     {
+        /// <summary>
+        /// Workout identifier.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Athlete identifier.
+        /// </summary>
         public int AthleteId { get; set; }
 
+        /// <summary>
+        /// Performed exerciceses with there coresponding data.
+        /// </summary>
         public IEnumerable<IntensityViewModel> Intensities { get; set; } = new List<IntensityViewModel>();
     }
 }
