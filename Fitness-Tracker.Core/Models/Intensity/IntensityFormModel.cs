2ea1303bd1712d1a3f40d172aae4e69547cc7297
Martin Mitev <martin.mitev.2403@gmail.com>
2025-03-08 08:59:47 +0200

    Updated comments for class IntensityFormModel.

--- Fitness-Tracker.Core/Models/Intensity/IntensityFormModel.cs
+++ Fitness-Tracker.Core/Models/Intensity/IntensityFormModel.cs
@@ -5,14 +5,29 @@
     /// </summary>
     public class IntensityFormModel
     {
+        /// <summary>
+        /// Exercise Identifier.
+        /// </summary>
         public int ExerciseId { get; set; }
 
+        /// <summary>
+        /// Integer for the used weight.
+        /// </summary>
         public int Weight { get; set; }
 
+        /// <summary>
+        /// Integer for the number of repetitions performed.
+        /// </summary>
         public int Reps { get; set; }
 
+        /// <summary>
+        /// Integer for the number of sets performed.
+        /// </summary>
         public int Sets { get; set; }
 
+        /// <summary>
+        /// Integer for the average time of a set.
+        /// </summary>
         public int Time { get; set; }
     }
 }
