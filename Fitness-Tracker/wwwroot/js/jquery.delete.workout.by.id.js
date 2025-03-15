e8599c11c4ecd064150ee873e6bf00a7b4f2057e
Martin Mitev <martin.mitev.2403@gmail.com>
2025-03-15 18:34:30 +0200

    Added jquery.delete.workout.js script to workout details

---
+++ Fitness-Tracker/wwwroot/js/jquery.delete.workout.by.id.js
@@ -0,0 +1,19 @@
+﻿$('#deleteWorkoutForm').submit(function (e) {
+    e.preventDefault();
+
+    var workoutId = $('#workoutId').val();
+
+    $.ajax({
+        url: getWorkoutIdURL,
+        type: 'POST',
+        data: {
+            Id: workoutId,
+        },
+        success: function (respons) {
+            $('#deleteWorkout').modal('hide');
+            location.href = "/Workout/All";
+        },
+        error: function (xhr, status, error) {
+        }
+    });
+});
